dcbbef8f88ad159b2cd6d7359226f856f9b83c75
Victoria Matynyan <the.strange.creature@yandex.ru>
2023-10-27 08:36:44 +0300

    refactoring: another auth method on backend to pass the tests

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -27,21 +27,38 @@ const { JWT_SECRET, NODE_ENV } = process.env;
 // };
 
 // авторизация на чистых куках (автотесты не пропускают)
+// module.exports = (req, res, next) => {
+//   let payload;
+//   try {
+//     const { cookies } = req;
+//     if ((cookies && cookies.jwt)) {
+//       const token = cookies.jwt;
+//       payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+//       req.user = payload;
+//       next();
+//     } else {
+//       next(new UnauthorizedError('Неверные авторизационные данные'));
+//     }
+//   } catch (error) {
+//     next(new UnauthorizedError('Неверные авторизационные данные'));
+//   }
+// };
+
 module.exports = (req, res, next) => {
-  let payload;
-  try {
-    const { cookies } = req;
-    if ((cookies && cookies.jwt)) {
-      const token = cookies.jwt;
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+  if (req.cookies.jwt) {
+    const token = req.cookies.jwt;
+    try {
+      const payload = jwt.verify(token, JWT_SECRET);
       req.user = payload;
-      next();
-    } else {
-      next(new UnauthorizedError('Неверные авторизационные данные'));
+    } catch (error) {
+      next(new UnauthorizedError('Необходима авторизация'));
+      return;
     }
-  } catch (error) {
-    next(new UnauthorizedError('Неверные авторизационные данные'));
+  } else {
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
+  next();
 };
 
 // на случай, если нужно будет откатиться на хранение токена в headers:
